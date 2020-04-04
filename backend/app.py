a568f3122d9f818d3ad2edd0fcbd1bdd77d6b611
Konrad Lisiecki <konradlisiecki@MacBook-Pro-Konrad.local>
2020-04-04 14:15:19 +0200

    upadte boilerplate

--- backend/app.py
+++ backend/app.py
@@ -1,28 +1,9 @@
 from flask import Flask
 from flask_cors import CORS 
-from flask import jsonify
-
-from api.api import api
-from api.models import db
-from api.config import Config 
+from flask import jsonify 
 
 import datetime
-
-# def create_app(config):
-#     app = Flask(__name__)
-#     CORS(app)
-#     # app.config.from_object(config)
-#     # register_extensions(app)
-#     return app
-
-
-# def register_extensions(app):
-#     api.init_app(app)
-#     db.init_app(app)
-
-# app = create_app(Config)
-
-
+ 
 app = Flask(__name__)
 CORS(app)
 
@@ -33,18 +14,3 @@ def time():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True, threaded=True)
-
-
-#     from flask import Flask, url_for
-# from flask import jsonify
-
-# import datetime
-
-# app = Flask(__name__)
- 
-# @app.route('/time')
-# def time():
-#     return jsonify({'time': datatime.datatime.now()})
- 
-# if __name__ == '__main__':
-#     app.run()
