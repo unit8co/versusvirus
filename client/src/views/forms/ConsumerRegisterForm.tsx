66abf5d5348001855b7e662d26afe0c469b8259f
Andrzej Skrodzki <andrzej@MacBook-Pro-Andrzej.local>
2020-04-04 15:20:39 +0200

    merge

---
+++ client/src/views/forms/ConsumerRegisterForm.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+
+export class ConsumerRegisterForm {
+    public render() {
+        <div className="consumer-register-form">
+            
+        </div>
+    }
+}
